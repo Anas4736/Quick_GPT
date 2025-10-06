a291927391a34d5138fba97f4df7b201b81fa73b
Dev <rastogi.db1@gmail.com>
2025-10-06 16:12:19 +0530

    Add changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPublishedImages, getUser, loginUser, registerUser } from "../controllers/usercontroller.js";
+import { getPublishedImages, getUser, loginUser, registerUser } from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
